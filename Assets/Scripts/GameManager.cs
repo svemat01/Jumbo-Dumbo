26d1482df038a3cb99fc58cc47f06df9d172aee6
svemat01 <jakob.h@hotmail.se>
2018-07-31 12:41:27 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //Variables
+    [Header("Variables")]
+    public GameObject player;
+    public GameObject gameover;
+
+    [Header("Respawn Position")]
+    public Vector3 RespawnPosition;
+
+    
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    //toggle buttons
+    public void Respawnbutton(bool buttonstoggle)
+    {
+        //player.GetComponent<CharacterController2D>().enabled = !buttonstoggle;
+        gameover.SetActive(buttonstoggle);
+        player.GetComponent<PlayerMovement>().enabled = !buttonstoggle;
+    }
+
+    // Teleport player to start/checkpoint position
+    void Respawn () {
+        player.transform.position = RespawnPosition;
+        Respawnbutton(false);
+	}
+}
---
+++ Assets/Scripts/Scripts for objects/MaceEnemyMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaceEnemyMovement : MonoBehaviour {
+
+    public float maxMoveDistance = 10;
+    //Set this to your objects initial position when game starts.
+    Vector3 origin;
+    public Vector3 destination;
+    public float speed = 10;
+    bool Up = true;
+
+    //Refrence to GameManager
+    public GameObject gamemanager;
+
+    float time = 0;
+
+    void Start()
+    {
+        origin = transform.position;
+        Debug.Log(transform.position);
+        StartCoroutine(GoDown());
+    }
+
+    IEnumerator GoDown()
+    {
+        
+        yield return new WaitForSecondsRealtime(1);
+        transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+
+    }
+
+    IEnumerator GoUp()
+    {
+        Debug.Log("up");
+        time = 5;
+        while (time > 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, origin, speed * Time.deltaTime);
+        }
+        yield return new WaitForSecondsRealtime(5);
+        time = 0;
+        StartCoroutine(GoDown());
+    }
+
+    void Update()
+    {
+        if (transform.position == origin)
+        {
+            Up = true;
+        } else if (transform.position == destination)
+        {
+            Up = false;
+        }
+
+
+        if (Up == true)
+        {
+            StartCoroutine(GoDown());
+        }
+        else if (Up == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, origin, speed * Time.deltaTime);
+        }
+    }
+
+    //Respawn player on collision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gamemanager.GetComponent<GameManager>().Respawnbutton(true);
+        }
+    }
+}
